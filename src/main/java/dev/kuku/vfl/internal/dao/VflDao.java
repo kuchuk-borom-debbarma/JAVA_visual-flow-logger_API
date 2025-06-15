1c49e6e733a55c2895808017107beda2bfba89aa
kuchukboramdebbarma <kuchukboromd@gmail.com>
2025-06-15 07:21:05 +0530

    Added sqlite for working with data

--- src/main/java/dev/kuku/vfl/internal/dao/VflDao.java
+++ src/main/java/dev/kuku/vfl/internal/dao/VflDao.java
@@ -6,9 +6,12 @@ import dev.kuku.vfl.api.models.VflLogDataType;
 import java.util.List;
 
 public interface VflDao {
-    /// Upsert to the database. <br>
-    /// Conflicting IDs will simply be ignored.<br>
-    /// Requires parentBlockID to be valid in database or included in the blocks to upsert.
+    /**
+     * Upsert blocks to datastore.<br>
+     * On ID conflicting, skip updating it.<br>
+     * For a block with parentBlockId, the ID must be of an existing block OR ID of a block that is being inserted.
+     * @param blocks blocks to insert
+     */
     void upsertBlocks(List<VflBlockDataType> blocks);
     /// Upsert to database.<br>
     /// Conflicting IDs will be ignored.<br>
