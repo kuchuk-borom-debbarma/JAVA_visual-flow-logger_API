87c1566fb2c96ee053e1f3138eb5a3680ac955f6
kuchukmobeet <kuchuk.mobeet@gmail.com>
2025-07-15 23:26:35 +0530

    added basic builder. Will make it better in future

--- src/main/java/dev/kuku/Main.java
+++ src/main/java/dev/kuku/Main.java
@@ -1,10 +1,16 @@
 package dev.kuku;
 
+import dev.kuku.vfl.core.builder.BlockLoggerBuilder;
+import dev.kuku.vfl.core.logger.BlockLogger;
+
 public class Main {
-    private static final ScopedValue vflScope = ScopedValue.newInstance();
+    private static final ScopedValue<BlockLogger> vflScope = ScopedValue.newInstance();
 
     public static void main(String... args) {
+        var a = BlockLoggerBuilder.create().rootBlock("GGEZ").apiUrl("GGEZ").build();
+        System.out.println(a);
     }
 }
 
 
+//TODO better safety using builder for everything
