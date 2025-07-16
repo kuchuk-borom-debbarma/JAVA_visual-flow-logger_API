76463d2ebd269c243ded6278fc79790abb55aa00
kuchukmobeet <kuchuk.mobeet@gmail.com>
2025-07-16 23:23:02 +0530

    Added call function

--- src/main/java/dev/kuku/vfl/executionLogger/ExecutionLoggerImpl.java
+++ src/main/java/dev/kuku/vfl/executionLogger/ExecutionLoggerImpl.java
@@ -2,6 +2,9 @@ package dev.kuku.vfl.executionLogger;
 
 import dev.kuku.vfl.core.BlockLog;
 
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+
 public class ExecutionLoggerImpl implements BlockLog {
 
     @Override
@@ -42,6 +45,26 @@ public class ExecutionLoggerImpl implements BlockLog {
     @Override
     public void runHere(String blockName, String message, Runnable runnable) {
 
+    }
+
+    @Override
+    public <T> T call(String blockName, String message, Function<T, String> endMessageFn, Callable<T> callable) {
+        return null;
+    }
+
+    @Override
+    public <T> T call(String blockName, String message, Callable<T> callable) {
+        return null;
+    }
+
+    @Override
+    public <T> T callHere(String blockName, String message, Function<T, String> endMessageFn, Callable<T> callable) {
+        return null;
+    }
+
+    @Override
+    public <T> T callHere(String blockName, String message, Callable<T> callable) {
+        return null;
     }
 
     @Override
