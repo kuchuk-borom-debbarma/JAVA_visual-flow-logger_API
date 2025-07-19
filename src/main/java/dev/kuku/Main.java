74708acde27d015f4708b449e295407cb01aac5c
kuchuk borom debbarma <45627511+kuchuk-borom-debbarma@users.noreply.github.com>
2025-07-19 18:45:54 +0530

    added two important todos

--- src/main/java/dev/kuku/Main.java
+++ src/main/java/dev/kuku/Main.java
@@ -3,6 +3,9 @@ package dev.kuku;
 public class Main {
 
     public static void main(String... args) {
-        System.out.println("Hello, World!");
+        //TODO Thread local chain based logger
+        //TODO Thread safe async buffer using executor provided by user
+        //TODO fluent API for pass through logger
+        //TODO simple annotation based logging.
     }
 }
