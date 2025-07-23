d8620b35b18393b37f3be791ee41327f9d95eb80
kuchuk borom debbarma <45627511+kuchuk-borom-debbarma@users.noreply.github.com>
2025-07-23 13:42:37 +0530

    added comment for next operation

--- src/main/java/dev/kuku/vfl/IPassthroughVFL.java
+++ src/main/java/dev/kuku/vfl/IPassthroughVFL.java
@@ -20,4 +20,13 @@ public interface IPassthroughVFL extends IVFL {
 
     <R> CompletableFuture<R> callAsync(String blockName, String message,
                                        Function<IPassthroughVFL, R> fn, Function<R, String> endMessageFn);
+
+    /*
+     * TODO branch for fire and forget or event
+     * The idea is to add a branch log to the sequence
+     * Then we can pass the branch log context to the event publisher.
+     * The event listener will then use it to create a sub block with secondary = true
+     *
+     * For fire and forget within the caller itself we can pass function itself and it will create the branch and the sub block with secondary true automatically
+     */
 }
