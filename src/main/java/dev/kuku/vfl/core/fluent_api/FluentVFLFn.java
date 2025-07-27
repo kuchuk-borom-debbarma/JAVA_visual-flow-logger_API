b704df28fbd1e4fb2a60db81c1d62a1b50467730
kuchuk borom debbarma <45627511+kuchuk-borom-debbarma@users.noreply.github.com>
2025-07-28 02:36:32 +0530

    Refactor StartBlockStep and FnStep classes to include message parameter for improved logging

--- src/main/java/dev/kuku/vfl/core/fluent_api/FluentVFLFn.java
+++ src/main/java/dev/kuku/vfl/core/fluent_api/FluentVFLFn.java
@@ -19,7 +19,7 @@ public class FluentVFLFn extends FluentVFL {
 
     public class StartBlockStep {
         private final String blockName;
-        private String message = null;
+        private String message;
 
         public StartBlockStep(String blockName) {
             this.blockName = blockName;
@@ -31,67 +31,72 @@ public class FluentVFLFn extends FluentVFL {
         }
 
         public <R> FnStep<R> forFunction(Function<VFLFn, R> fn) {
-            return new FnStep<>(fn);
+            return new FnStep<>(blockName, message, fn);
         }
 
-        public FnStepVoid forFunction(Consumer<VFLFn> fn) {
-            return new FnStepVoid(fn);
+        public FnStepVoid forConsumer(Consumer<VFLFn> fn) {
+            return new FnStepVoid(blockName, message, fn);
+        }
+    }
+
+    public class FnStep<R> {
+        private final String blockName;
+        private final String message;
+        private final Function<VFLFn, R> fn;
+        private Function<R, String> endMsg;
+
+        private FnStep(String blockName, String message, Function<VFLFn, R> fn) {
+            this.blockName = blockName;
+            this.message = message;
+            this.fn = fn;
         }
 
-        public class FnStep<R> {
-            private final Function<VFLFn, R> fn;
-            private Function<R, String> endMsg = null;
-
-            public FnStep(Function<VFLFn, R> fn) {
-                this.fn = fn;
-            }
-
-            public FnStep<R> withEndMessage(Function<R, String> endMessageSerializer) {
-                this.endMsg = endMessageSerializer;
-                return this;
-            }
-
-            public R executeAsPrimary() {
-                return vfl.callPrimarySubBlock(blockName, message, fn, endMsg);
-            }
-
-            public R executeAsJoiningSecondary() {
-                return vfl.callSecondaryJoiningBlock(blockName, message, fn, endMsg);
-            }
-
-            public R executeAsNonJoiningSecondary() {
-                return vfl.callSecondaryNonJoiningBlock(blockName, message, fn, endMsg);
-            }
-
+        public FnStep<R> withEndMessage(Function<R, String> endMessageSerializer) {
+            this.endMsg = endMessageSerializer;
+            return this;
         }
 
-        public class FnStepVoid {
-            private final Consumer<VFLFn> fn;
+        public R executeAsPrimary() {
+            return vfl.callPrimarySubBlock(blockName, message, fn, endMsg);
+        }
 
-            public FnStepVoid(Consumer<VFLFn> fn) {
-                this.fn = fn;
-            }
+        public R executeAsJoiningSecondary() {
+            return vfl.callSecondaryJoiningBlock(blockName, message, fn, endMsg);
+        }
 
-            public void executeAsPrimary() {
-                vfl.callPrimarySubBlock(blockName, message, o -> {
-                    fn.accept(o);
-                    return null;
-                }, null);
-            }
+        public R executeAsNonJoiningSecondary() {
+            return vfl.callSecondaryNonJoiningBlock(blockName, message, fn, endMsg);
+        }
+    }
 
-            public void executeAsJoiningSecondary() {
-                vfl.callSecondaryJoiningBlock(blockName, message, o -> {
-                    fn.accept(o);
-                    return null;
-                }, null);
-            }
+    public class FnStepVoid {
+        private final String blockName;
+        private final String message;
+        private final Consumer<VFLFn> fn;
 
-            public void executeAsNonJoiningSecondary() {
-                vfl.callSecondaryNonJoiningBlock(blockName, message, o -> {
-                    fn.accept(o);
-                    return null;
-                }, null);
-            }
+        private FnStepVoid(String blockName, String message, Consumer<VFLFn> fn) {
+            this.blockName = blockName;
+            this.message = message;
+            this.fn = fn;
+        }
+
+        private Function<VFLFn, Void> toVoidFunction() {
+            return vflFn -> {
+                fn.accept(vflFn);
+                return null;
+            };
+        }
+
+        public void executeAsPrimary() {
+            vfl.callPrimarySubBlock(blockName, message, toVoidFunction(), null);
+        }
+
+        public void executeAsJoiningSecondary() {
+            vfl.callSecondaryJoiningBlock(blockName, message, toVoidFunction(), null);
+        }
+
+        public void executeAsNonJoiningSecondary() {
+            vfl.callSecondaryNonJoiningBlock(blockName, message, toVoidFunction(), null);
         }
     }
 }
