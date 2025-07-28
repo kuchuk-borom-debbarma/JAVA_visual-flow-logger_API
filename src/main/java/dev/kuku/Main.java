9c3e275ce71c81fbc6c1dbb0af5f4c00ffa8e909
kuchuk borom debbarma <45627511+kuchuk-borom-debbarma@users.noreply.github.com>
2025-07-28 19:00:07 +0530

    Update project version to 0.0.1 and add server submodule

--- src/main/java/dev/kuku/Main.java
+++ src/main/java/dev/kuku/Main.java
@@ -3,9 +3,6 @@ package dev.kuku;
 public class Main {
 
     public static void main(String... args) {
-        //TODO Thread local chain based logger
-        //TODO Thread safe async buffer using executor provided by user
-        //TODO fluent API for pass through logger
         //TODO simple annotation based logging.
         //TODO configuration using spring or file {@link https://claude.ai/chat/28eca1e0-9d4a-4465-836a-5d1feed5a3c4}
     }
