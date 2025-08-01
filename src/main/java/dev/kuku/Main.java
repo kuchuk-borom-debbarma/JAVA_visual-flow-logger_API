360d5dc1abd13c662c21ccb04cb7a154da59f495
kuchuk borom debbarma <45627511+kuchuk-borom-debbarma@users.noreply.github.com>
2025-08-01 17:49:38 +0530

    added todos to vfl java and implemented block post functionalirtyies

--- src/main/java/dev/kuku/Main.java
+++ src/main/java/dev/kuku/Main.java
@@ -9,6 +9,8 @@ public class Main {
         //TODO create abstract class for vfl buffer and flush handler that will do the logical building while allowing abstract methods to be overridden
         //TODO Progressive logging with fixed format which can be used to then generate different output. for client only. VFL Hub can still focus purely on its own logic. But the VFL Hub needs to support importing the client log
         //Flush at interval
+        //TODO ability to run a function and log a runnable
+        //TODO remove having to do Instanee.Runner
         //TODO logger can take string and args of param to fill in those {}
         //TODO configuration using spring or file {@link https://claude.ai/chat/28eca1e0-9d4a-4465-836a-5d1feed5a3c4}
 
