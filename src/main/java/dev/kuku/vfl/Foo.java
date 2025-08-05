5b38ea0590c7ff4fc0ee9989cbc1291534577186
kuchuk borom debbarma <kuchuk.mobeet@gmail.com>
2025-08-06 01:21:44 +0530

    Annotation seems working

--- src/main/java/dev/kuku/vfl/Foo.java
+++ src/main/java/dev/kuku/vfl/Foo.java
@@ -1,6 +1,10 @@
 package dev.kuku.vfl;
 
+import dev.kuku.vfl.core.helpers.Util;
+import dev.kuku.vfl.impl.threadlocal.VFLAnnotationCompletableFuture;
 import dev.kuku.vfl.impl.threadlocal.VFLBlock;
+
+import java.util.concurrent.ExecutionException;
 
 public class Foo {
     public static void publicFoo() {
@@ -11,10 +15,24 @@ public class Foo {
     private static void foo() {
         //their code
         bar();
+        var t1 = VFLAnnotationCompletableFuture.runAsync(() -> {
+            bar();
+        });
+        var t2 = VFLAnnotationCompletableFuture.runAsync(() -> {
+            bar();
+        });
+        try {
+            t1.get();
+            t2.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        bar();
     }
 
     @VFLBlock
     public static void bar() {
         //Their code
+        System.out.println("Bar in " + Util.getThreadInfo());
     }
 }
