c2d31fba7114d7eab2823b6165971a88445a54ea
kuchuk borom debbarma <kuchukboromd@outlook.com>
2025-08-06 09:42:32 +0530

    added todo

--- src/main/java/dev/kuku/Main.java
+++ src/main/java/dev/kuku/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String... args) {
         //TODO code should not break even if ThreadAnnotation is disabled when using logger
         //TODO esay way to skip using the logger
+        //todo exceotion advice snnotation
     }
 }
