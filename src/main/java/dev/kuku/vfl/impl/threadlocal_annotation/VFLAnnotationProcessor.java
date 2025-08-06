1a58f002cb786ae8302d79ada1f80a6e600c517c
kuchuk borom debbarma <kuchukboromd@outlook.com>
2025-08-06 17:54:25 +0530

    Defined VFL annotation

--- src/main/java/dev/kuku/vfl/impl/threadlocal_annotation/VFLAnnotationProcessor.java
+++ src/main/java/dev/kuku/vfl/impl/threadlocal_annotation/VFLAnnotationProcessor.java
@@ -1,5 +1,55 @@
 package dev.kuku.vfl.impl.threadlocal_annotation;
 
+import dev.kuku.vfl.core.buffer.VFLBuffer;
+import dev.kuku.vfl.impl.threadlocal_annotation.annotations.ThreadVFLAdviceData;
+import dev.kuku.vfl.impl.threadlocal_annotation.annotations.ThreadVFLAnnotationAdvice;
+import dev.kuku.vfl.impl.threadlocal_annotation.annotations.ThreadVFLAnnotationProcessor;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.matcher.ElementMatchers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.instrument.Instrumentation;
+
 public class VFLAnnotationProcessor {
-    //TODO byte buddy init
+    public static Logger log = LoggerFactory.getLogger(ThreadVFLAnnotationProcessor.class);
+
+    public static volatile boolean initialized = false;
+
+    public static synchronized void initialise(VFLBuffer buffer, boolean disable) {
+        if (disable) {
+            log.debug("Disable flag is true, skipping instrumentation");
+            return;
+        }
+        if (initialized) {
+            log.debug("[VFL] Agent already initialised – skipping");
+            return;
+        }
+
+        try {
+            Instrumentation inst = ByteBuddyAgent.install();
+            ThreadVFLAdviceData.buffer = buffer;
+
+            new AgentBuilder.Default().with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
+                    .type(ElementMatchers.declaresMethod(ElementMatchers.isAnnotatedWith(VFLBlock.class)))
+                    .transform((b, td, cl, jm, pd) ->
+                            b.visit(Advice.to(ThreadVFLAnnotationAdvice.class)
+                                    .on(ElementMatchers.isAnnotatedWith(VFLBlock.class)))
+                    )
+                    .installOn(inst);
+
+            initialized = true;
+            log.debug("[VFL] Instrumentation initialised successfully");
+        } catch (Exception e) {
+            log.error("[VFL] Initialisation failed", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static synchronized void reset() {
+        initialized = false;
+        ThreadVFLAdviceData.buffer = null;
+    }
 }
--- src/main/java/dev/kuku/vfl/impl/threadlocal_annotation/VFLFutures.java
+++ src/main/java/dev/kuku/vfl/impl/threadlocal_annotation/VFLFutures.java
@@ -1,4 +1,79 @@
 package dev.kuku.vfl.impl.threadlocal_annotation;
 
+import dev.kuku.vfl.core.models.logs.enums.LogTypeBlockStartEnum;
+import dev.kuku.vfl.impl.threadlocal_annotation.annotations.SpawnedThreadContext;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.function.Supplier;
+
+import static dev.kuku.vfl.core.helpers.Util.GetThreadInfo;
+
+@Slf4j
 public class VFLFutures {
+
+    public static <R> CompletableFuture<R> supplyAsync(Supplier<R> supplier, Executor executor) {
+        SpawnedThreadContext spawnedThreadContext = new SpawnedThreadContext(
+                ContextManager.getCurrentContext(),
+                LogTypeBlockStartEnum.SUB_BLOCK_START_SECONDARY_JOIN
+        );
+
+        return CompletableFuture.supplyAsync(() -> {
+            var newThreadCtx = ContextManager.spawnedThreadContext;
+            if (newThreadCtx.get() != null) {
+                log.warn("Spawned Thread Context for executor is not null! {}", GetThreadInfo());
+            }
+            ContextManager.spawnedThreadContext.set(spawnedThreadContext);
+            return supplier.get();
+        }, executor);
+    }
+
+    public static <R> CompletableFuture<R> supplyAsync(Supplier<R> supplier) {
+        SpawnedThreadContext spawnedThreadContext = new SpawnedThreadContext(
+                ContextManager.getCurrentContext(),
+                LogTypeBlockStartEnum.SUB_BLOCK_START_SECONDARY_JOIN
+        );
+
+        return CompletableFuture.supplyAsync(() -> {
+            var newThreadCtx = ContextManager.spawnedThreadContext;
+            if (newThreadCtx.get() != null) {
+                log.warn("Spawned Thread Context for default executor is not null! {}", GetThreadInfo());
+            }
+            ContextManager.spawnedThreadContext.set(spawnedThreadContext);
+            return supplier.get();
+        });
+    }
+
+    public static CompletableFuture<Void> runAsync(Runnable runnable, Executor executor) {
+        SpawnedThreadContext spawnedThreadContext = new SpawnedThreadContext(
+                ContextManager.getCurrentContext(),
+                LogTypeBlockStartEnum.SUB_BLOCK_START_SECONDARY_JOIN
+        );
+
+        return CompletableFuture.runAsync(() -> {
+            var newThreadCtx = ContextManager.spawnedThreadContext;
+            if (newThreadCtx.get() != null) {
+                log.warn("Spawned Thread Context for executor is not null! {}", GetThreadInfo());
+            }
+            ContextManager.spawnedThreadContext.set(spawnedThreadContext);
+            runnable.run();
+        }, executor);
+    }
+
+    public static CompletableFuture<Void> runAsync(Runnable runnable) {
+        SpawnedThreadContext spawnedThreadContext = new SpawnedThreadContext(
+                ContextManager.getCurrentContext(),
+                LogTypeBlockStartEnum.SUB_BLOCK_START_SECONDARY_JOIN
+        );
+
+        return CompletableFuture.runAsync(() -> {
+            var newThreadCtx = ContextManager.spawnedThreadContext;
+            if (newThreadCtx.get() != null) {
+                log.warn("Spawned Thread Context for default executor is not null! {}", GetThreadInfo());
+            }
+            ContextManager.spawnedThreadContext.set(spawnedThreadContext);
+            runnable.run();
+        });
+    }
 }
